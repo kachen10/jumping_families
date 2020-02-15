a88f5b8bee9255a0002cf4d48c47f8db4a9b44f5
Karen Chen <karencyuan@gmail.com>
2020-02-15 02:08:13 -0800

    Main menu UI

---
+++ jumping_families/src/title_screen/MainMenu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/TextureRect/VBoxContainer
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	pass
