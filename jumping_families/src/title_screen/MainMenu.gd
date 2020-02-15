c48b1358f85d788f5ba6501ccb07b87136e769e4
Karen Chen <karencyuan@gmail.com>
2020-02-15 02:31:30 -0800

    Wire buttons in main menu

--- jumping_families/src/title_screen/MainMenu.gd
+++ jumping_families/src/title_screen/MainMenu.gd
@@ -2,8 +2,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/TextureRect/VBoxContainer
+	$MarginContainer/VBoxContainer/VBoxContainer/NewGame.grab_focus()
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	pass
+	if $MarginContainer/VBoxContainer/VBoxContainer/NewGame.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/NewGame.grab_focus()
+	if $MarginContainer/VBoxContainer/VBoxContainer/Exit.is_hovered():
+		$MarginContainer/VBoxContainer/VBoxContainer/Exit.grab_focus()
+
+
+func _on_NewGame_pressed():
+	get_tree().change_scene("res://src/levels/Level1.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
