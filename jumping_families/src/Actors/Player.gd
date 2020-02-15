ad3e170db93a060958cf59eedf749adad8d1c45b
cl418 <crystalliang882@gmail.com>
2020-02-15 07:30:59 -0800

    Have multiple characters

--- jumping_families/src/Actors/Player.gd
+++ jumping_families/src/Actors/Player.gd
@@ -1,27 +1,31 @@
 extends Actor
 
 export var inViewport: bool
+var frozen = true;
 
 func _on_player_exited(viewport: Viewport) -> void:
-	hide()
+	frozen = true
 	inViewport = false
 
 func _on_Player_entered(viewport: Viewport) -> void:
-	show()
+	frozen = false
 	inViewport = true
 
 func _ready() -> void:
-	hide()
+	show()
+	if position.y > 1200:
+		frozen = false;
 
 func _physics_process(delta: float) -> void:
-	var is_jump_interrupted := Input.is_action_just_released("jump") and _velocity.y < 0.0
-	var direction: = get_direction()
-	if Input.is_action_just_released("jump"):
-		set_collision_mask_bit(2, false)
-	elif _velocity.y > 0:
-		set_collision_mask_bit(2, true)
-	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	if !frozen:
+		var is_jump_interrupted := Input.is_action_just_released("jump") and _velocity.y < 0.0
+		var direction: = get_direction()
+		if Input.is_action_just_released("jump"):
+			set_collision_mask_bit(2, false)
+		elif _velocity.y > 0:
+			set_collision_mask_bit(2, true)
+		_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+		_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
 	var movement_val = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
